66cace15320f61e272b1aaa560c9fb1b4ddc589d
itsMatrik <itsmatrik@gmail.com>
2024-01-15 18:20:03 +0300

    some changes..

--- src/components/startGame/startGame.jsx
+++ src/components/startGame/startGame.jsx
@@ -4,8 +4,7 @@ import styles from './startGame.module.css'
 const StartGame = ({setGameStart}) => {
     return (
         <div className={styles.container}>
-            <img className={styles.img} src={'https://live.staticflickr.com/7271/7131884899_f99efd9b76_b.jpg'}/>
-            <h1 className={styles.h1}>Опрос от Джейсона Стэтхэма по белорусскому языку</h1>
+            <h1 className={styles.h1}>Апытанне па 16-18 параграфе</h1>
             <button className={styles.button} onClick={() => setGameStart(false)}>Начать</button>
         </div>
     );
